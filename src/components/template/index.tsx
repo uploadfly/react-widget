035a771002fcbdc751a3453cef96279f92d3499c
yhoungdev <yhoungdev@gmail.com>
2023-08-12 18:32:13 +0100

    feat: added dropzone

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -1,16 +1,23 @@
 import Button from "../atom/button/Button";
 import Modal from "../organism/modal/modal";
+import { useDropzone } from "react-dropzone";
+import { useCallback } from "react";
 
 const IndexPage = () => {
+  const onDropFile = useCallback((acceptCallback) => {
+    console.log(acceptCallback);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone(onDropFile);
+
   return (
     <div className="my-4">
       <center>
         <Modal>
-          <div className="text-center">
-            Click to upload
-            <center>
-              <Button>Upload</Button>
-            </center>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+
+            {isDragActive ? <p> droped file </p> : <p>Show item</p>}
           </div>
         </Modal>
         <Button>Upload Now</Button>
