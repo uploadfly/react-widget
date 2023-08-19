aea2aefc44bfc30ff70ce3fa56a47717b994fe29
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-19 13:16:57 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { MdUpload } from "react-icons/md";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <UfWidget apiKey="uf_e9f7ab5db69c4952b35151b72f89a609">
+    <div className="flex justify-between py-5 px-10">
+      <h1>Uploadfly</h1>
+      <UfWidget apiKey="">
         <button className="bg-orange-500 flex items-center gap-2 px-8 py-3 rounded-full text-white font-semibold">
           <MdUpload />
           Upload
