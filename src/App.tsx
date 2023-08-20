6c5f00afd7482ec47d120264b1f76a2172624d55
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-20 03:07:30 +0100

    removed API key

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ const App = () => {
   return (
     <div className="flex justify-between py-5 px-10">
       <h1>Uploadfly</h1>
-      <UfWidget
-        apiKey="uf_e9f7ab5db69c4952b35151b72f89a609"
-        accentColor="#f945ab"
-      >
+      <UfWidget apiKey="" accentColor="#f945ab">
         <button className="bg-orange-500 flex items-center gap-2 px-8 py-3 rounded-full text-white font-semibold">
           <MdUpload />
           Upload
