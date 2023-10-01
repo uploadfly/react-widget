cd7f8f0aa9638d4b179778cb82fbb5331acccf05
akinloluwami <akinkunmioye42@gmail.com>
2023-10-01 12:16:30 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import { UfWidget } from ".";
-import { MdUpload } from "react-icons/md";
-
 const App = () => {
-  return (
-    <div className="flex justify-between py-5 px-10">
-      <h1>Uploadfly</h1>
-      <UfWidget apiKey="" accentColor="#f945ab">
-        <button className="bg-orange-500 flex items-center gap-2 px-8 py-3 rounded-full text-white font-semibold">
-          <MdUpload />
-          Upload
-        </button>
-      </UfWidget>
-    </div>
-  );
+  return <></>;
 };
 
 export default App;
